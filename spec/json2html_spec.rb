6dd6751e108e17e1023d24f2b2c42c3070c26d3d
Gareth Smyth <smyckers@gmail.com>
2015-08-05 16:08:21 +0100

    Updating to use xpath to test the output which should be a little less brittle - still may not be ideal but no other plans to change

--- spec/json2html_spec.rb
+++ spec/json2html_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'nokogiri'
 
 describe Json2Html do
   it 'has a version number' do
@@ -7,57 +8,67 @@ describe Json2Html do
 
   it 'should convert json number node to a div with id' do
     html = subject.to_html('{"my_value":13.5}')
-    expect(html).to eq('<div id="my_value">13.5</div>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//div[id="my_value"]/text()').text).to eq('13.5')
   end
 
   it 'should convert json text node to a div with id' do
     html = subject.to_html('{"my_value":"some text"}')
-    expect(html).to eq('<div id="my_value">some text</div>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//div[id="my_value"]/text()').text).to eq('some text')
   end
 
   it 'should convert null json into empty div with id' do
     html = subject.to_html('{"my_value":null}')
-    expect(html).to eq('<div id="my_value"></div>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//div[id="my_value"]/text()').text).to eq('')
   end
 
   it 'should convert true json value into div with id and value true' do
     html = subject.to_html('{"my_value":true}')
-    expect(html).to eq('<div id="my_value">true</div>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//div[id="my_value"]/text()').text).to eq('true')
   end
 
   it 'should convert false json value into div with id and value false' do
     html = subject.to_html('{"my_value":false}')
-    expect(html).to eq('<div id="my_value">false</div>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//div[id="my_value"]/text()').text).to eq('false')
   end
 
   it 'should create unordered list of primitive htmls with indexed ids for json array' do
     html = subject.to_html('{"my_value":["string", 2.4, false]}')
-    expect(html).to eq('<ul id="my_value"><li><div id="my_value_1">string</div></li>'\
-                       '<li><div id="my_value_2">2.4</div></li><li><div id="my_value_3">false</div></li></ul>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//ul[id="my_value"]/li/div[id="my_value_1"]/text()').text).to eq('string')
+    expect(parsed_html.css('//ul[id="my_value"]/li/div[id="my_value_2"]/text()').text).to eq('2.4')
+    expect(parsed_html.css('//ul[id="my_value"]/li/div[id="my_value_3"]/text()').text).to eq('false')
   end
 
   it 'should create a div with internal divs each with unique ids for a json object' do
     html = subject.to_html('{"my_value":{"child1":"a", "child2":"b"}}')
-    expect(html).to eq('<div id="my_value"><div id="my_value_child1">a</div><div id="my_value_child2">b</div></div>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//div[id="my_value"]/div[id="my_value_child1"]/text()').text).to eq('a')
+    expect(parsed_html.css('//div[id="my_value"]/div[id="my_value_child2"]/text()').text).to eq('b')
   end
 
   it 'should create a complex structure' do
     html = subject.to_html('{"child1":{"grandchild1":"value1","grandchild2":[6,7,8]},
                              "child2":[{"grandchild3":"value2" }]}')
-    expect(html).to eq('<div id="child1">'\
-                       '<div id="child1_grandchild1">value1</div>'\
-                       '<ul id="child1_grandchild2">'\
-                       '<li><div id="child1_grandchild2_1">6</div></li>'\
-                       '<li><div id="child1_grandchild2_2">7</div></li>'\
-                       '<li><div id="child1_grandchild2_3">8</div></li>'\
-                       '</ul>'\
-                       '</div>'\
-                       '<ul id="child2">'\
-                       '<li>'\
-                       '<div id="child2_1">'\
-                       '<div id="child2_1_grandchild3">value2</div>'\
-                       '</div>'\
-                       '</li>'\
-                       '</ul>')
+
+    parsed_html = Nokogiri::HTML.parse(html)
+    expect(parsed_html.css('//div[id="child1"]/div[id="child1_grandchild1"]/text()').text).to eq('value1')
+    expect(parsed_html.css('//div[id="child1"]/ul[id="child1_grandchild2"]')).not_to be nil
+    expect(parsed_html.css('//li/div[id="child1_grandchild2_1"]/text()').text).to eq('6')
+    expect(parsed_html.css('//li/div[id="child1_grandchild2_2"]/text()').text).to eq('7')
+    expect(parsed_html.css('//li/div[id="child1_grandchild2_3"]/text()').text).to eq('8')
+    expect(parsed_html.css('//ul[id="child2"]/li/div[id="child2_1"]')).not_to be nil
+    expect(parsed_html.css('//div[id="child2_1_grandchild3"]/text()').text).to eq('value2')
   end
 end
